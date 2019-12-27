d75d69e67ed1a681cf514b630e7804f7b75c38c5
guxingke <admin@guxingke.com>
2019-12-28 00:44:09 +0800

    :recycle: mv asm toy to tools

--- src/main/java/com/gxk/jvm/Main.java
+++ src/main/java/com/gxk/jvm/Main.java
@@ -1,7 +1,5 @@
 package com.gxk.jvm;
 
-import com.gxk.jvm.ext.bc.ByteCodeGenerator;
-import com.gxk.jvm.ext.bc.ByteCodeInterpreter;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,12 +9,6 @@ public class Main {
   public static void main(String[] args)  {
     if (args.length == 0) {
       printHelp();
-      return;
-    }
-
-    // special case
-    if (args[0].startsWith("--")) {
-      doSpecialCase(args);
       return;
     }
 
@@ -34,43 +26,6 @@ public class Main {
 
     VirtualMachine vm = new VirtualMachine();
     vm.run(cmd);
-  }
-
-  // 一些有趣的玩意儿
-  private static void doSpecialCase(String[] args) {
-    // parse args
-    switch (args[0]) {
-      case "--":
-        // byte code interpreter
-        String file = args[1];
-
-        if (args.length == 2) {
-          ByteCodeInterpreter.interpreter(file);
-          break;
-        }
-
-        int size = args.length - 2;
-        String[] args2 = new String[size];
-        System.arraycopy(args, 2, args2, 0, args2.length);
-
-        Integer[] pargs = new Integer[size];
-        for (int i = 0; i < args2.length; i++) {
-          pargs[i] = Integer.parseInt(args2[i]);
-        }
-
-        ByteCodeInterpreter.interpreter(file, pargs);
-        break;
-      case "--bc":
-        String clazzPath = args[1];
-        String methodName = "bc";
-        if (args.length == 3) {
-          methodName = args[2];
-        }
-
-        ByteCodeGenerator.gen(clazzPath, methodName);
-        break;
-
-    }
   }
 
   private static void printHelp() {
