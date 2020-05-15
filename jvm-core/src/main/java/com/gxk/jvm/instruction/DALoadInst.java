8facb64c0b932ee2fe5db7bef9a4b1d45500170d
guxingke <admin@guxingke.com>
2020-05-15 23:09:19 +0800

    :sparkles: use fake heap

--- jvm-core/src/main/java/com/gxk/jvm/instruction/DALoadInst.java
+++ jvm-core/src/main/java/com/gxk/jvm/instruction/DALoadInst.java
@@ -1,10 +1,8 @@
 package com.gxk.jvm.instruction;
 
 import com.gxk.jvm.rtda.Frame;
-import com.gxk.jvm.rtda.Slot;
 import com.gxk.jvm.rtda.memory.Heap;
 import com.gxk.jvm.rtda.memory.KArray;
-import com.gxk.jvm.rtda.memory.KField;
 
 public class DALoadInst implements Instruction {
 
@@ -12,15 +10,7 @@ public class DALoadInst implements Instruction {
   public void execute(Frame frame) {
     Integer index = frame.popInt();
     KArray array = (KArray) Heap.load(frame.popRef());
-    Long item = array.items[index];
-    KField field = Heap.load(item).getField("value", "D");
-    Slot[] slots = field.val;
-    Integer high = slots[index].num;
-    Integer low = slots[index + 1].num;
-
-    long l1 = (high & 0x000000ffffffffL) << 32;
-    long l2 = low & 0x00000000ffffffffL;
-    long tmp = l1 | l2;
-    frame.pushDouble(Double.longBitsToDouble(tmp));
+    Double val = (Double) array.primitiveItems[index];
+    frame.pushDouble(val);
   }
 }
--- jvm-core/src/main/java/com/gxk/jvm/instruction/FAStoreInst.java
+++ jvm-core/src/main/java/com/gxk/jvm/instruction/FAStoreInst.java
@@ -1,10 +1,8 @@
 package com.gxk.jvm.instruction;
 
 import com.gxk.jvm.rtda.Frame;
-import com.gxk.jvm.rtda.Slot;
 import com.gxk.jvm.rtda.memory.Heap;
 import com.gxk.jvm.rtda.memory.KArray;
-import com.gxk.jvm.rtda.memory.MethodArea;
 
 public class FAStoreInst implements Instruction {
 
@@ -13,9 +11,6 @@ public class FAStoreInst implements Instruction {
     Float val = frame.popFloat();
     Integer index = frame.popInt();
     KArray array = (KArray) Heap.load(frame.popRef());
-    Long offset = MethodArea.findClass("java/lang/Float").newObject();
-    int tmp = Float.floatToIntBits(val);
-    Heap.load(offset).setField("value", "F", new Slot[]{new Slot(tmp, Slot.FLOAT)});
-    array.items[index] = offset;
+    array.primitiveItems[index] = val;
   }
 }
